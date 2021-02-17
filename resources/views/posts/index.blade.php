eca0b6f60e5eb1c1a704d9e77819f1ce46c90ad8
EC2 Default User <ec2-user@ip-172-31-19-16.us-east-2.compute.internal>
2021-02-17 11:39:43 +0000

    Laravel基礎課題３

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts')
+
+@section('content')
+
+ <div class="container mt-4">
+        <div class="card mt-4">
+            
+            <div class="card-header mb-2">
+                Title
+            </div>
+            <div class="card-body">
+                <p class="card-text">
+                    body
+                </p>
+            </div>
+            <div class="card-footer">
+                <span　class="mr-2">
+                    投稿日時
+                </span>
+            </div>
+            
+        </div>
+    </div>
+
+@endsection('content')
